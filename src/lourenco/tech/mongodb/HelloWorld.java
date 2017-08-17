d6cc92598c91cecd16f60567a59c3eb40d693c80
Lourenco <gon.lourenco@gmail.com>
2017-08-18 00:01:35 +0100

    First commit

--- src/lourenco/tech/mongodb/HelloWorld.java
+++ src/lourenco/tech/mongodb/HelloWorld.java
@@ -1,5 +1,46 @@
 package lourenco.tech.mongodb;
 
-public class HelloWorld {
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        MongoClient mongoClient = new MongoClient("localhost");
+        MongoDatabase database = mongoClient.getDatabase("dbname");
+        MongoCollection<Document> collection = database.getCollection("collectionname");
+
+        Document exampleDoc = new Document();
+        exampleDoc.put("name","Lourenco");
+        exampleDoc.put("country","USA");
+
+        Document exampleDoc2 = new Document();
+        exampleDoc2.put("name","John");
+        exampleDoc2.put("country","USA");
+
+        List<Document> multipleDocs = new ArrayList<>();
+        multipleDocs.add(exampleDoc);
+        multipleDocs.add(exampleDoc2);
+
+        collection.insertMany(multipleDocs);
+
+        FindIterable<Document> result =  collection.find();
+
+        for(Document docFound : result){
+            System.out.println(docFound.toJson());
+        }
+
+        Document filter = new Document("name","Lourenco");
+
+        FindIterable<Document> result2 =  collection.find(filter);
+
+        for(Document docFound : result2){
+            System.out.println(docFound.toJson());
+        }
+    }
 }
