087535d24fcb0f71cea425a07690c81fd45b2e52
Lourenco <gon.lourenco@gmail.com>
2017-08-18 11:41:55 +0100

    Comments added

--- src/lourenco/tech/mongodb/HelloWorld.java
+++ src/lourenco/tech/mongodb/HelloWorld.java
@@ -11,10 +11,12 @@ import java.util.List;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        //Connect to MongoDB
         MongoClient mongoClient = new MongoClient("localhost");
         MongoDatabase database = mongoClient.getDatabase("dbname");
         MongoCollection<Document> collection = database.getCollection("collectionname");
 
+        //Create some example data
         Document exampleDoc = new Document();
         exampleDoc.put("name","Lourenco");
         exampleDoc.put("country","USA");
@@ -27,16 +29,19 @@ public class HelloWorld {
         multipleDocs.add(exampleDoc);
         multipleDocs.add(exampleDoc2);
 
+        //Insert data in Collection
         collection.insertMany(multipleDocs);
 
+        //Get all entries from Collection
         FindIterable<Document> result =  collection.find();
 
         for(Document docFound : result){
             System.out.println(docFound.toJson());
         }
 
+        //Create filter
         Document filter = new Document("name","Lourenco");
-
+        //Find specific entry
         FindIterable<Document> result2 =  collection.find(filter);
 
         for(Document docFound : result2){
